dc371d311e2397654f1eed98a61d255823ced54e
PeterAnyaduba <pnanyaduba@cbn.gov.ng>
2023-10-14 21:25:36 +0100

    day one build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      's2.coinmarketcap.com',
+      'sr3.coinmarketcap.com',
+      'encrypted-tbn0.gstatic.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
